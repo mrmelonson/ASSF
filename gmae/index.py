abc616abfea93ffe76a73fd47c733662084bc0fe
Zelenyy <dangerdude119@gmail.com>
2018-05-10 12:03:43 +1000

    Updated Index.py
    > updates

--- gmae/index.py
+++ gmae/index.py
@@ -2,15 +2,32 @@
 #12/04/18
 #A small snowy forest
 #made in python 3
-from stuff import player, printing
+
+from stuff import player as playerpg, printing, explore
 
 printing.PrintTitle()
+printing.Print("You wake up, your head is hurting and you are cold")
 printing.Print("You struggle to remember your own name..")
-printing.Print("Thats right it's:")
 
 name = input()
-player = player.Player(name)
-printing.Print("you open your eyes and look around")
+player = playerpg.Players(name)
+
+printing.Print("Thats right it's " + player.name)
+printing.Print("You look around")
+printing.Print("Nothing but trees and snow")
+
+#fight.Fight("Wolf", 20, 2)
+
+answer = ''
+
+while answer != 'y' and answer != 'n':
+    printing.PrintOpt("Should you go exploring? (y/n)")
+    answer = input()
+
+if answer == 'y':
+    explore.Explore()
+else:
+    explore.Idle()
 
 
 
